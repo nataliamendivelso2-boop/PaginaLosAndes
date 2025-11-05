9b69620d0eab44f5f08ee73ff057b146ba978f2b
David Zamora <d.zamorac@uniandes.edu.co>
2025-11-05 12:39:54 -0500

    .

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -71,7 +71,7 @@ const NavBar = () => {
             Servicios
           </a>
           <a
-            href="#galeria"
+            href="#galerias"
             onClick={(event) => handleSmoothScroll(event, '#galeria')}
             className="transition-all duration-200 hover:text-white hover:drop-shadow-[0_2px_6px_rgba(6,182,212,0.5)]"
           >
