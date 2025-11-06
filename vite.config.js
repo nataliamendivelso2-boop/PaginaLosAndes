29d4f77b65bd8fe83f9cbabcc6f33daefc2c87de
nataliamendivelso2-boop <nataliamendivelso2@gmail.com>
2025-11-06 18:34:14 -0500

    cambios

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import tailwind from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Mueve la caché fuera de node_modules para evitar bloqueos en Windows/OneDrive
+  cacheDir: '.vite',
   plugins: [react(), tailwind()],
 })
