7e367c4b3d3bda4e5b581cd6285922671c293e2d
nataliamendivelso2-boop <nataliamendivelso2@gmail.com>
2025-11-07 11:49:00 -0500

    cambios

--- src/utils/seo.js
+++ src/utils/seo.js
@@ -1,4 +1,4 @@
 export const SITE_URL = 'https://losandes-soacha.com';
-export const OG_IMAGE_URL = `${SITE_URL}/og-image.png`;
+export const OG_IMAGE_URL = `/icon-512.png`;
 export const BUSINESS_ID = `${SITE_URL}/#dentist`;
 export const WEBSITE_ID = `${SITE_URL}/#website`;
